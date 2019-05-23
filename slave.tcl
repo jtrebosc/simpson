9fc6a4f58a1b98aef56bc4b71039f733069033c4
Julien TREBOSC <julien.trebosc@univ-lille.fr>
2019-05-23 15:48:38 +0200

    regenerate tclcode.c file from .tcl files This will make addition of new tcl functions easier CMakeList.txt is updated for automatic generation this uses bash scripts Tested on linux only

---
+++ slave.tcl
@@ -0,0 +1,26 @@
+#
+#   In slaves we do not need to evaluate spinsys nor par
+#
+
+proc putmatrix {m {fm "%9.3g"}} {
+   foreach i $m {
+     foreach j $i {
+        if {[llength $j] == 2} {
+          puts -nonewline [format "($fm,$fm) " [lindex $j 0] [lindex $j 1]]
+          puts -nonewline [format "($fm,$fm) " [li
+        } else {
+          puts -nonewline [format $fm $j]
+        }
+     }
+     puts ""
+   }
+}
+
+
+proc par {data} {  
+
+}
+
+proc spinsys {data} {  
+
+}
