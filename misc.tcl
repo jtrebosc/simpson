b87633e667a7d6eedc01efec0fe883326c4398ef
Julien TREBOSC <julien.trebosc@univ-lille.fr>
2019-05-24 22:37:10 +0200

    features : add several utility functions to simpson :
    - nucspin {nuc}: returns the spin value of <nuc>
    - nucNspinsys {N} : returns the nucleus at position N in spinsys:nuclei section
    - spin_nucNspinsys {N} : returns the spin of nucleus at position N in spinsys:nuclei
    - nucQIS {nuc} {Cq} {eta} {n=-0.5} {m=0.5} : returns the QIS value depending on
    nuc spin Cq, eta, n, m where n and m define the transition <n|m>,
    <n|m> default to <-1/2|+1/2> which is the central transition
    value is calculated according to par(proton_frequency)
    - QISspinsys {N} : calculates QIS for nucleus at position N in spinsys(nuclei)
    and using quadrupole N line for Cq and eta values

--- misc.tcl
+++ misc.tcl
@@ -144,3 +144,60 @@ proc simview {args} {
   puts "Saved files: $args"
 }
 
+
+proc nucNspinsys {N} {
+  global spinsys
+  set nucs $spinsys(nuclei)
+  set num_nucs [llength $nucs]
+  if {$num_nucs < $N || $num_nucs <1}  {
+    puts "spin_of_spinsys_nuc: nucleus number $N is not between 1 and max nucleus number=$num_nucs"
+    exit 1
+  }
+  return [lindex $nucs [expr $N-1]]
+}
+
+proc spin_nucNspinsys {N} {
+  set nuc [nucNspinsys $N]
+  return [nucspin $nuc]
+}
+
+proc nucQIS {nuc Cq eta {n -0.5} {m 0.5}} {
+#calculates quadrupolar induced shift from isotope <nuc>, Cq, eta, and transition <n|m>
+# CT corresponds to <-0.5|0.5>, ST1 would be <0.5|1.5>
+# this requires to know the larmor frequency
+  global par
+  set I [nucspin $nuc] 
+  if {$I == 0.5} {
+    puts "Warning $nuc has spin 1/2. QIS is 0"
+    return 0
+  }
+  if  {[array names par -exact proton_frequency] == ""} {
+    puts "proton_frequency in par section is required"
+    exit 1
+  }
+  set nu0 [resfreq $nuc $par(proton_frequency)]
+  set QIS [expr -pow(3.*$Cq/(2.*$I*(2.*$I-1)),2)*56./(5040.*$nu0)*(3.+$eta*$eta)*($m*($I*($I+1.)-3.*$m*$m)-$n*($I*($I+1.)-3.*$n*$n))]
+  return $QIS
+}
+
+proc QISspinsys {N {n -0.5} {m 0.5}} {
+# calculate QIS for spin N in channel according to quadrupole parameters defined in spinsys  transition <n|m>
+#<n|m> default is <-0.5|0.5> corresponding to central transition
+  global spinsys
+  set nuc [nucNspinsys $N]
+  set Cq X
+  set eta X
+  foreach quad_par [array names spinsys quadrupole] {
+    if {[lindex $spinsys($quad_par) 0] == $N} {
+      set Cq [lindex $spinsys($quad_par) 2]
+      set eta [lindex $spinsys($quad_par) 3]
+      break
+    }
+  }
+  if {$Cq == "X"} {
+    puts "Warning no quadrupole coupling defined for nucleus $N=$nuc."
+    set Cq 0
+    set eta 0
+  }
+  return [nucQIS $nuc $Cq $eta $n $m]
+}
