cf862e027ccc39bc3c8b819a6f6ac483c1a419aa
Julien TREBOSC <julien.trebosc@univ-lille.fr>
2020-05-19 22:57:12 +0200

    BUG (slave.tcl): fix typo in putmatrix

--- slave.tcl
+++ slave.tcl
@@ -7,7 +7,6 @@ proc putmatrix {m {fm "%9.3g"}} {
      foreach j $i {
         if {[llength $j] == 2} {
           puts -nonewline [format "($fm,$fm) " [lindex $j 0] [lindex $j 1]]
-          puts -nonewline [format "($fm,$fm) " [li
         } else {
           puts -nonewline [format $fm $j]
         }
